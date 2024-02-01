50fad21292797175d112690aa1d83e2c03cf67d3
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2024-02-01 11:15:47 +0100

    modification ajout favoris

--- ajouterFavorisGroupe.php
+++ ajouterFavorisGroupe.php
@@ -1,8 +1,6 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id_groupe = $_POST['id_groupe'];
+    $id_groupe = $_GET['id'] ?? 1;
     $id_utilisateur = $_SESSION['user'].getIdUtilisateur(); 
     $data = new Data\DataBase();
     $userStatement = $data->insertFavoriteGroupe($id_groupe, $id_utilisateur);
-}
 ?>
--- ajouterFavorisPlaylist.php
+++ ajouterFavorisPlaylist.php
@@ -1,8 +1,6 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id_playlist = $_POST['id_playlist'];
+    $id_playlist = $_GET['id'] ?? 1;
     $id_utilisateur = $_SESSION['user'].getIdUtilisateur(); 
     $data = new Data\DataBase();
     $userStatement = $data->insertFavorisPlaylist($id_playlist, $id_utilisateur);
-}
 ?>
