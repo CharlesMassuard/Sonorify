e3d363de7bf8ee831b3ae914b4ec47d4e8532ea9
Baptiste <baptiste.haudebourg37@orange.fr>
2024-02-02 21:17:00 +0100

    suppression des playlist dans les favoirs

---
+++ supprimerFavorisPlaylist.php
@@ -0,0 +1,8 @@
+<?php
+    $id_playlist = $_GET['id'] ?? 1;
+    $id_utilisateur = $_SESSION['user']['id_utilisateur'] ?? 1;
+    require_once 'Data/DataBase.php'; 
+    $data = new Data\DataBase();
+    $userStatement = $data->deleteFavorisPlaylist($id_playlist, $id_utilisateur);
+    header('Location: playlist.php?id='.$id_playlist);
+?>
