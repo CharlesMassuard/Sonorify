d2d1ee46bbd3cbe049e1ec475dc86485c85f8685
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2024-02-02 20:30:26 +0100

    Ajout cover, titre, artistes/album/annee

--- player.php
+++ player.php
@@ -14,12 +14,21 @@
                 <div id="circle_progress"></div>
             </div>
         </div>
-        <div class="controls">
-            <button id="prevButton"><i class="material-icons">skip_previous</i></button>
-            <button id="playButton"><i class="material-icons">play_arrow</i></button>
-            <button id="nextButton"><i class="material-icons">skip_next</i></button>
-            <div id="currentTimeDisplay">--:-- / --:--</div>
-            <input type="range" id="volumeSlider" min="0" max="1" step="0.01" value="1">
+        <div class="infos">
+            <div class="controls">
+                <button id="prevButton"><i class="material-icons">skip_previous</i></button>
+                <button id="playButton"><i class="material-icons">play_arrow</i></button>
+                <button id="nextButton"><i class="material-icons">skip_next</i></button>
+                <div id="currentTimeDisplay">--:-- / --:--</div>
+            </div>
+            <div id="infos_music">
+                <img id="cover" src="./ressources/images/TerryAllen_Juarez.jpg" alt="cover">
+                <div id='infos_ecrites'>
+                    <h4 id="title">Finale</h4>
+                    <div class="infos_supplementaires">Gareth Coker • ARK: Genesis Part 2 (Original Soundtrack) • 2021</div>
+                </div>
+            </div>
+        <!-- <input type="range" id="volumeSlider" min="0" max="1" step="0.01" value="1"> -->
         </div>
     </div>
 </body>
