af5a8e6ae7de286d1a8dadf937e98d24dfad559c
Baptiste <baptiste.haudebourg37@orange.fr>
2024-02-04 13:37:52 +0100

    correction ajouterFavorisPlaylist.php

--- ajouterFavorisPlaylist.php
+++ ajouterFavorisPlaylist.php
@@ -1,6 +1,8 @@
 <?php
     $id_playlist = $_GET['id'] ?? 1;
-    $id_utilisateur = $_SESSION['user'].getIdUtilisateur(); 
+    $id_utilisateur = $_SESSION['user']['id_utilisateur'] ?? 1;
+    require_once 'Data/DataBase.php'; 
     $data = new Data\DataBase();
     $userStatement = $data->insertFavorisPlaylist($id_playlist, $id_utilisateur);
+    header('Location: playlist.php?id='.$id_playlist);
 ?>
