c05e8f4b76406bb427b59c72b3414d112d48b2a9
Baptiste <baptiste.haudebourg37@orange.fr>
2024-02-04 13:38:17 +0100

    ajout ajouterNotePlaylist.php

---
+++ ajouterNotePlaylist.php
@@ -0,0 +1,9 @@
+<?php
+    $id_playlist = $_GET['id'] ?? 1;
+    $note = $_GET['note'] ?? 1;
+    $id_utilisateur = $_SESSION['user']['id_utilisateur'] ?? 1;
+    require_once 'Data/DataBase.php'; 
+    $data = new Data\DataBase();
+    $userStatement = $data->insertNotePlaylist($id_playlist, $id_utilisateur, $note);
+    header('Location: playlist.php?id='.$id_playlist);
+?>
