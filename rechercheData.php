ec739cbaf113012f30901e544c3be95181ace775
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2024-02-17 13:00:39 +0100

    Réduction resultats musiques 6 -> 4

--- rechercheData.php
+++ rechercheData.php
@@ -7,25 +7,27 @@ function rechercheData($data) {
     $database = new Data\DataBase();
     $groupes = $database->getGroupesByName( $data ?? null);
     $resultats = [];
-    $resultats['groupes'] = array_slice($groupes, 0, 6);
+    $resultats['groupes'] = array_slice($groupes, 0, 4);
     if ($groupes !== null && is_array($groupes) && count($groupes)  == 1){
-        $resultats['albums'] = array_slice($database->getAlbumsByIdGroupe($groupes[0]['id_groupe']), 0, 6);
-        $musiques = array_slice($database->getMusiquesByName($data), 0, 6);
-        $musiquesGroupe = array_slice($database->getMusiquesByIdGroupe($groupes[0]['id_groupe']), 0, 6);
+        $resultats['albums'] = array_slice($database->getAlbumsByIdGroupe($groupes[0]['id_groupe']), 0, 4);
+        $musiques = array_slice($database->getMusiquesByName($data), 0, 4);
+        $musiquesGroupe = array_slice($database->getMusiquesByIdGroupe($groupes[0]['id_groupe']), 0, 4);
         foreach($musiquesGroupe as $musiqueGroupe) {
             if($musiqueGroupe['titre'] != $data) {
                 $musiques[] = $musiqueGroupe;
             }
         }
+        // Limit the total number of musics to 4
+        $musiques = array_slice($musiques, 0, 4);
         $resultats['musiques'] = $musiques;
     } else {
-        $playlists = array_slice($database->getPlaylistsByName($data), 0, 6);
-        $genres = array_slice($database->getGenresByName($data), 0, 6);
-        $albums = array_slice($database->getAlbumsByName($data), 0, 6);
+        $playlists = array_slice($database->getPlaylistsByName($data), 0, 4);
+        $genres = array_slice($database->getGenresByName($data), 0, 4);
+        $albums = array_slice($database->getAlbumsByName($data), 0, 4);
         $resultats['albums'] = $albums;
         $resultats['playlists'] = $playlists;
         $resultats['genres'] = $genres;
-        $musiques = array_slice($database->getMusiquesByName($data), 0, 6);
+        $musiques = array_slice($database->getMusiquesByName($data), 0, 4);
         $resultats['musiques'] = $musiques;
     }
     print_r(json_encode($resultats));
